a7a84715ca15f2685dfc8f4a49cbc1de513e6017
William Hu <williamhu@williams-mbp.lan>
2026-01-14 23:45:43 -0800

    using local host 0.0.0.0

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ app.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="127.0.0.1", port=7860, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=7860, reload=True)
